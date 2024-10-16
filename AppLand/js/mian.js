5819ac3b2b9007be5ac4ba432acb08f0811e16ae
MaxRichard2007 <max536854@gmail.com>
2024-10-16 20:16:48 +0330

    BC1-7-feat: Add responsiveness to header

---
+++ AppLand/js/mian.js
@@ -0,0 +1,9 @@
+const sidebar = document.querySelector(".hiddenHeader");
+
+function showSidebar() {
+  sidebar.style.display = "block";
+}
+
+function closeSidebar() {
+  sidebar.style.display = "none";
+}
