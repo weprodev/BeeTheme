6fbda4633c150df806df20ccec5b5371d068b351
MaxRichard2007 <max536854@gmail.com>
2024-10-19 18:52:36 +0330

    BC1-7-docs: Base-structure-for-AppLand

--- AppLand/js/mian.js
+++
@@ -1,9 +0,0 @@
-const sidebar = document.querySelector(".hiddenHeader");
-
-function showSidebar() {
-  sidebar.style.display = "block";
-}
-
-function closeSidebar() {
-  sidebar.style.display = "none";
-}
