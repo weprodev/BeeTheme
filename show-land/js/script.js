e5e5f5eed9a56e8d297dc73a6c3b95f50d3abba5
sadiGFG <sadikarimi2008@gmail.com>
2024-11-21 21:00:25 +0330

    BC1-69-fix: move showland js code to main.js

--- show-land/js/script.js
+++ show-land/js/script.js
@@ -1,10 +1,3 @@
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".header");
-
-btnNavEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open");
-});
-
 /*------------------------------------*\
   #DATA
 \*------------------------------------*/
