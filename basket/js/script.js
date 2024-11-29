d821f1b520a6f4cbf01d998c8ad9e0acb2654e4c
ahmadmohammadirad2006 <ahmadmohammadirad2006@gmail.com>
2024-11-29 12:06:26 +0330

    BC1-89-feat: Add the functionality of closing and opening select platform of Basket

--- basket/js/script.js
+++ basket/js/script.js
@@ -2,7 +2,6 @@ $("#icon-close-page").on("click", function () {
   $("#sec-1").addClass("blur");
   $("#close-page").removeClass("hidden");
 });
-
 $("#close").on("click", function () {
   $("#close-page").addClass("hidden");
   $("#sec-1").removeClass("blur");
@@ -13,4 +12,11 @@ $("#no-button").on("click", function () {
 });
 $("#back-main-page2").on("click", function () {
   $("#sec-2").removeClass("right-0");
+});
+
+$("#yes-button").on("click", function () {
+  $("#sec-3").addClass("left-0");
+});
+$("#back-main-page1").on("click", function () {
+  $("#sec-3").removeClass("left-0");
 });
