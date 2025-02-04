b775bac0dbf85877961d956bc62fbeb4a51076e3
sadiGFG <sadikarimi2008@gmail.com>
2025-02-05 00:12:30 +0330

    174-feat: Convert global phenomenon section to React with styled-components and implement responsive design

---
+++ apps/react/product-details/layout/Phenomenon.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Section, Heading, Paragraph } from "./Phenomenon.styles";
+const Phenomenon: React.FC = () => {
+  return (
+    <>
+      <Section>
+        <Heading>A Truly Global Phenomenon</Heading>
+        <Paragraph>
+          Elementor has crossed all boundaries and borders, reaching WordPress
+          users around the globe. It’s available in over 50 languages, offers
+          complete RTL support,compatible with translation plugins like WPML and
+          Polylang.
+        </Paragraph>
+      </Section>
+    </>
+  );
+};
+
+export default Phenomenon;
